7678a369ef5a5f02560f82d276c6a57cb6cdb01f
Tanaphol SUEBCHUA <b4lmung.m@gmail.com>
2024-01-13 16:53:04 +0900

    [REF] implement utilities funcs for config

--- src/config.rs
+++ src/config.rs
@@ -7,3 +7,35 @@ pub struct AppConfig{
     pub port: Option<u16>,
     pub max_tables: Option<i16>
 }
+
+
+impl AppConfig{
+
+    pub fn new_from_env() -> Self{
+        dotenvy::dotenv().ok();
+        envy::from_env::<Self>().unwrap_or(
+            Self { 
+                db_url: None,
+                host: None, 
+                port: None, 
+                max_tables: None 
+        })
+    }
+
+    pub fn get_db_url(&self) -> String{
+        self.db_url.clone().unwrap_or(r#"postgres://postgres:password@localhost/test"#.to_string())
+    }
+
+    pub fn get_max_table(&self) -> i16{
+        self.max_tables.unwrap_or(100)
+    }
+
+    pub fn get_host(&self) -> String{
+        self.host.clone().unwrap_or("localhost".to_string())
+    }
+
+    pub fn get_port(&self) -> u16{
+        self.port.unwrap_or(3000)
+    }
+}
+
