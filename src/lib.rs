fe159c99a3b310a28e930d001e9115c1f09a0117
Tanaphol SUEBCHUA <b4lmung.m@gmail.com>
2024-01-13 23:20:02 +0900

    Move run server code from Main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-pub mod config;
 pub mod model;
 pub mod error;
 pub mod dao;
 pub mod context;
 pub mod handlers;
 pub mod routes;
+pub mod config;
+
+use tokio::net::TcpListener;
+
+pub async fn run_server() -> Result<(), error::ApiError>{
+    let config = config::AppConfig::new_from_env();
+    let host = config.get_host();
+    let port = config.get_port();
+
+    let database_url =  config.get_db_url();
+
+    tracing::info!("HOST:{host} PORT:{port}  DB URL:{database_url}");
+
+    let context = context::ApiContext::new_from_env().await?;
+
+    let router = routes::app(context);
+    let listener = TcpListener::bind(format!("{host}:{port}")).await
+                                    .expect(&format!("Unable to bind server to {host}:{port}"));
+
+    tracing::info!("Server is ready");
+    axum::serve(listener, router).await.expect("Cannot serve service");
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
-use restaurant_server::error::ApiError;
-use restaurant_server::{ context, routes, config};
+use rusty::error::ApiError;
 use sqlx::error::*;
-use tokio::net::TcpListener;
 
 
+use rusty::run_server;
 
 #[tokio::main]
 async fn main() -> Result<(), ApiError>{
-    let config = config::AppConfig::new_from_env();
-    let host = config.get_host();
-    let port = config.get_port();
-
-    let database_url =  config.get_db_url();
-
-    tracing::info!("HOST:{host} PORT:{port}  DB URL:{database_url}");
-
-    let context = context::ApiContext::new_from_env().await?;
-
-    let router = routes::app(context);
-    let listener = TcpListener::bind(format!("{host}:{port}")).await
-                                    .expect(&format!("Unable to bind server to {host}:{port}"));
-
-    tracing::info!("Server is ready");
-    axum::serve(listener, router).await.expect("Cannot serve service");
-    Ok(())
+    tracing_subscriber::fmt().with_thread_names(true).init();
+    run_server().await
 }
